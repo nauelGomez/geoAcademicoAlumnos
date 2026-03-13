68c21ffa9bbb8602010ab36da73b3fc06a4800dc
nauelGomez <139215988+nauelGomez@users.noreply.github.com>
2026-03-12 23:08:06 -0300

    a

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/clear', function() {
+    \Illuminate\Support\Facades\Artisan::call('route:clear');
+    return 'Rutas limpias';
+});
